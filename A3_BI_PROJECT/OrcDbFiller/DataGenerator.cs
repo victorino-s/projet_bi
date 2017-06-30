a1d380e52cbe74f276d5113a6957bb014997d704
Victorino <victorino_s@outlook.com>
2017-06-30 16:15:45 +0200

    Added C# Proj (Generator) + json files

---
+++ A3_BI_PROJECT/OrcDbFiller/DataGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace OrcDbFiller
+{
+    public static class DataGenerator
+    {
+        public static void Generate_CandyData(out dynamic candyList)
+        {
+            List<string> _styles = new List<string>();
+            using(StreamReader sr = new StreamReader("../../_local_data/styles_bonbons.json"))
+            {
+                string json = sr.ReadToEnd();
+                JsonFileContentObj obj = JsonConvert.DeserializeObject<JsonFileContentObj>(json);
+
+                foreach(string s in obj.content)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            candyList = _styles;
+            Console.WriteLine("Done");
+        }
+
+        public static void Generate_OrderData()
+        {
+
+        }
+    }
+}
---
+++ A3_BI_PROJECT/OrcDbFiller/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrcDbFiller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic test;
+            DataGenerator.Generate_CandyData(out test);
+        }
+    }
+}
